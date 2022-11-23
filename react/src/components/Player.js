6f488c3c55e6e6d3bb8a03aaa96a739d3e02441b
MonsterBasket <blazza1@hotmail.com>
2022-11-24 01:55:55 +1100

    Player animations were mostly working, and then not
    And somehow they took out player movement with them...

--- react/src/components/Player.js
+++ react/src/components/Player.js
@@ -1,59 +1,31 @@
 // import image from "../images/main-character.png"
 import { useEffect, useState } from "react";
 
-function Player(){
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  let keys = [];
-  let velocity = [0,0];
-  const maxSpeed = 3;
-
-  useEffect(() => {
-    window.addEventListener("keydown", keyDown);
-    window.addEventListener("keyup", keyUp );
-    gameLoop();
-    return () => {
-      window.removeEventListener("keydown", keyDown);
-      window.removeEventListener("keyup", keyUp);
-    };
-  }, [])
-
-  function keyDown(e){
-    keys[e.code] = true;
+function selectAnimation(x, y, velocityX, velocityY, lastDirection){
+  const obj = {
+    transform: `translate(${x}px, ${y}px) scale(3)`,
+    animationName: ""
   }
-  function keyUp(e){
-    keys[e.code] = false;
+  if(velocityX == 0 && velocityY == 0){
+    if(lastDirection == "KeyW") obj.animationName = "idleUp";
+    if(lastDirection == "KeyS") obj.animationName = "idleDown";
+    if(lastDirection == "KeyA") obj.animationName = "idleLeft";
+    if(lastDirection == "KeyD") obj.animationName = "idleRight";
   }
-
-  function handleInput(){
-    if(keys["KeyA"] && velocity[0] > -maxSpeed) velocity[0] -= 0.15;
-    if(keys["KeyD"] && velocity[0] <  maxSpeed) velocity[0] += 0.15;
-    if(keys["KeyS"] && velocity[1] <  maxSpeed) velocity[1] += 0.15;
-    if(keys["KeyW"] && velocity[1] > -maxSpeed) velocity[1] -= 0.15;
-    if(!keys["KeyA"] && !keys["KeyD"]){
-      if((velocity[0] < 0.1 && velocity[0] > 0) || (velocity[0] > -0.1 && velocity[0] < 0)) velocity[0] = 0;
-      if(velocity[0] < 0) velocity[0] += 0.1;
-      if(velocity[0] > 0) velocity[0] -= 0.1;
-    } 
-    if(!keys["KeyW"] && !keys["KeyS"]){
-      if((velocity[1] < 0.1 && velocity[1] > 0) || (velocity[1] > -0.1 && velocity[1] < 0)) velocity[1] = 0;
-      if(velocity[1] < 0) velocity[1] += 0.1;
-      if(velocity[1] > 0) velocity[1] -= 0.1;
-    } 
+  else if(Math.abs(velocityX) > Math.abs(velocityY)){
+   obj.animationName = velocityX > 0 ? "walkRight" : "walkLeft";
   }
-
-  function gameLoop() {
-    handleInput();
-    if(velocity[0]) setX(prev => prev + velocity[0])
-    if(velocity[1]) setY(prev => prev + velocity[1])
-    requestAnimationFrame(gameLoop);
+  else if(Math.abs(velocityX) < Math.abs(velocityY)){
+    obj.animationName = velocityY > 0 ? "walkDown" : "walkUp";
   }
+  return obj
+}
 
-  const characterMover = {
-    transform: `translate(${x}px, ${y}px) scale(3)`
-  }
+function Player({x, y, velocityX, velocityY, lastDirection}){
+  console.log(x, y, velocityX, velocityY, lastDirection)
+  const style = selectAnimation(x, y, velocityX, velocityY, lastDirection);
 
-  return <div id="player" style={characterMover}></div>
+  return <div id="player" style={style}></div>
 }
 
 export default Player;
--- react/src/components/World.js
+++ react/src/components/World.js
@@ -7,19 +7,55 @@ import Player from "./Player"
 function MapMaker(){
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
+  const [lastDirection, setDirection] = useState("");
+  const [velocityX, setVelX] = useState(0)
+  const [velocityY, setVelY] = useState(0)
+  let keys = [];
+  const maxSpeed = 3;
   const mapSize = [40,23]; //I'll extract this from individual map details later
 
   useEffect(() => {
-    window.addEventListener("keydown", handleInput);
-    return () => {window.removeEventListener("keydown", handleInput);};
+    window.addEventListener("keydown", keyDown);
+    window.addEventListener("keyup", keyUp );
+    gameLoop();
+    return () => {
+      window.removeEventListener("keydown", keyDown);
+      window.removeEventListener("keyup", keyUp);
+    };
   }, [])
 
-  function handleInput(e){
-    if(e.key === "ArrowRight") return setX(prev=>prev + 16);
-    if(e.key === "ArrowLeft") return setX(prev=>prev - 16);
-    if(e.key === "ArrowUp") return setY(prev=>prev - 16);
-    if(e.key === "ArrowDown") setY(prev=>prev + 16);
+  function keyDown(e){
+    keys[e.code] = true;
   }
+  function keyUp(e){
+    keys[e.code] = false;
+    setDirection(e.code);
+  }
+
+  function handleInput(){
+    if(keys["KeyA"] && velocityX > -maxSpeed) setVelX(prev => prev -= 0.15);
+    if(keys["KeyD"] && velocityX <  maxSpeed) setVelX(prev => prev += 0.15);
+    if(keys["KeyS"] && velocityY <  maxSpeed) setVelY(prev => prev += 0.15);
+    if(keys["KeyW"] && velocityY > -maxSpeed) setVelY(prev => prev -= 0.15);
+    if(!keys["KeyA"] && !keys["KeyD"]){
+      if((velocityX < 0.1 && velocityX > 0) || (velocityX > -0.1 && velocityX < 0)) setVelX(0);
+      if(velocityX < 0) setVelX(prev => prev += 0.1);
+      if(velocityX > 0) setVelX(prev => prev -= 0.1);
+    } 
+    if(!keys["KeyW"] && !keys["KeyS"]){
+      if((velocityY < 0.1 && velocityY > 0) || (velocityY > -0.1 && velocityY < 0)) setVelY(0);
+      if(velocityY < 0) setVelY(prev => prev += 0.1);
+      if(velocityY > 0) setVelY(prev => prev -= 0.1);
+    } 
+  }
+
+  function gameLoop() {
+    handleInput();
+    if(velocityX) setX(prev => prev + velocityX)
+    if(velocityY) setY(prev => prev + velocityY)    
+    requestAnimationFrame(gameLoop);
+  }
+
   const worldMover = {
     left: `${0}px`,
     top: `${0}px`
@@ -34,7 +70,7 @@ function MapMaker(){
         <World0101 />
     </div>
     <div>
-      <Player />
+      <Player x={x} y={y} velocityX={velocityX} velocityY={velocityY} lastDirection={lastDirection}/>
     </div>
   </div>
 }
